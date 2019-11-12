a1389fc72917f7cbd3e3ddab37d6584dba83279d
[InfoDefenseResearch] <[dpjacko@yahoo.com]>
2019-11-11 19:45:41 -0600

    First Commit CharGen.py

---
+++ README.md
@@ -0,0 +1,14 @@
+# Simple Character Generator
+---
+
+## Purpose 
+
+This is a simple (ish) python program that will create a select number of characters or numbers in a resulting string.  This is only a test program so that I can get used to the uses of GitHub.
+
+## Date 
+11/11/2019
+
+## Revisions 
+none 
+
+
---
+++ py_passgen.py
@@ -0,0 +1,85 @@
+#passgen#
+import sys, random, string
+
+def lowercase():
+    while True:
+        try:
+            char_set = string.ascii_lowercase
+            result = ''.join(random.sample(char_set*6, 8))
+            print(result)
+        except (KeyboardInterrupt):
+            exit()
+def lowerupper():
+    while True:
+        try:
+            char_set = string.ascii_lowercase + string.ascii_uppercase
+            result = ''.join(random.sample(char_set*6, 8))
+            print(result)
+        except (KeyboardInterrupt):
+            exit()
+def lowernum():
+    while True:
+        try:
+            char_set = string.ascii_lowercase + string.digits
+            result = ''.join(random.sample(char_set*6, 8))
+            print(result)
+        except (KeyboardInterrupt):
+            exit()
+def uppernum():
+    while True:
+        try:
+            char_set = string.ascii_uppercase + string.digits
+            result = ''.join(random.sample(char_set*6, 8))
+            print(result)
+        except (KeyboardInterrupt):
+            exit()
+def loweruppernum():
+    while True:
+        try:
+            char_set = string.ascii_uppercase + string.ascii_lowercase + string.digits
+            result = ''.join(random.sample(char_set*6, 8))
+            print(result)
+        except (KeyboardInterrupt):
+            exit()
+def uppercase():
+    while True:
+        try:
+            char_set = string.ascii_uppercase
+            result = ''.join(random.sample(char_set*6, 8))
+            print(result)
+        except (KeyboardInterrupt):
+            exit()
+def arglist():
+    print('options: -l lowercase, -lU lower and uppercase, -l1 lower and numerals, -U upper ascii, -U1 upper and numerals, -lU1 lower, upper, and numerals, -C [char] [num] custom character set and length, --help this list')
+
+args = sys.argv[1:]
+if args:
+    for arg in args:
+        if arg == '-l':
+            lowercase()
+        elif arg == '-lU':
+            lowerupper()
+        elif arg == '-l1':
+            lowernum()
+        elif arg == '-U1':
+            uppernum()
+        elif arg == '-lU1':
+            loweruppernum()
+        elif arg == '-U':
+            uppercase()
+        elif arg == '--help':
+            arglist()
+        elif arg == '-C':
+            while True:
+                try:
+                    char_set = sys.argv[2]
+                    char_len = sys.argv[3]
+                    result = ''.join([random.choice(char_set) for _ in range(int(char_len))])
+                    print (result)
+                except (ValueError):
+                    print('ValueError: sample larger than population')
+                    exit()
+                except (KeyboardInterrupt):
+                    exit()
+else:
+    arglist()
